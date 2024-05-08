79ccf5e3f24868d00a559b5c98f91ea679f3fa88
VINAYVARMAGIT <169148572+VINAYVARMAGIT@users.noreply.github.com>
2024-05-08 16:45:38 +0530

    2nd commit

--- src/Static_Arrays/Remove_Duplicates_From_Sorted_Array.java
+++ src/Static_Arrays/Remove_Duplicates_From_Sorted_Array.java
@@ -1,5 +1,9 @@
 package Static_Arrays;
 
 public class Remove_Duplicates_From_Sorted_Array {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        
+    }
 
 }
